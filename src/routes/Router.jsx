6f320438adf2a087e4f251f7f2235ee25f607794
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2024-04-27 18:54:16 +0330

    set ternery Authorization  user

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, } from 'react-router-dom';
 import NotFound from 'src/pages/404';
 import AdminPage from 'src/pages/AdminPage';
 import AuthPage from 'src/pages/AuthPage';
@@ -9,16 +9,15 @@ import HomePage from 'src/pages/HomePage';
 import { getProfileUser } from 'src/services/user';
 
 const Routers = () => {
-
     const {data,isLoading,error}=useQuery(["profile"],getProfileUser)
     console.log('data',data);
     return (
         <>
             <Routes>
                 <Route index element={<HomePage />} />
-                <Route path="auth" element={<AuthPage />} />
-                <Route path="admin" element={<AdminPage />} />
-                <Route path="dashboard" element={<DashboardPage />} />
+                <Route path="auth" element={data? <Navigate to="/dashboard" /> : <AuthPage />} />
+                <Route path="admin" element={data && data.data.roule=='ADMIN'? <AdminPage /> : <Navigate to="/" />} />
+                <Route path="dashboard" element={data? <DashboardPage /> : <Navigate to="/auth" />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
           
