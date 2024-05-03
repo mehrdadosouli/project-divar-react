0cd4ee5ef72d13e72a5cfd644ff6adde5fd56d4c
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2024-05-03 19:48:24 +0330

    update code delete item category

--- src/components/template/GetCategoryList.jsx
+++ src/components/template/GetCategoryList.jsx
@@ -1,12 +1,25 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { useEffect } from 'react';
-import { getCategory } from 'src/services/admin';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import { deleteCategory ,getCategory} from 'src/services/admin';
 
 const GetCategoryList = () => {
-    const {data , isLoading}=useQuery(['get-category'],getCategory)
-    useEffect(()=>{
-
-    },[])
+    const queryClient=useQueryClient()
+    const {data}=useQuery(['get-category'],getCategory)
+    const [info,setInfo]=useState()
+    const { mutate } = useMutation(deleteCategory,{
+        onSuccess:()=>{
+            queryClient.invalidateQueries(['get-category'])
+        }
+    })
+    const deleteHandler = (event,id) => {
+        event.preventDefault()
+        const findId = data?.data.find(i => i._id === id)
+        if (findId) {
+            mutate(findId._id)
+        } else {
+            return
+        }
+    }
     return (
         <div className='p-10'>
             {
@@ -16,9 +29,13 @@ const GetCategoryList = () => {
                         <p>{elem.name}</p>
                     </div>
                     <div>
+                        <button className='p-2 bg-red-300 mx-5 rounded-xl text-white' onClick={(event)=>deleteHandler(event,elem._id)}>DELETE</button>
                         <span>slug: {elem.slug}</span>
                     </div>
                 </div>)) : ""
+            }
+            {
+
             }
         </div>
     );
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
-import styles from './admin.module.css'
 import CategoryForm from 'src/components/template/CategoryForm';
 import GetCategoryList from 'src/components/template/GetCategoryList';
-import DeleteCategoryForm from 'src/components/template/DeleteCategoryForm';
 const AdminPage = () => {
 
     return (
         <>
            <GetCategoryList />
            <CategoryForm /> 
-           <DeleteCategoryForm />
         </>
     );
 };
